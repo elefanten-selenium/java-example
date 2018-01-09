0ff07cd5732c79d5a5f866e4927c6e91ce99715d
Dmitry Ushakov <dmitry.ushakov@taskdata.ru>
2018-01-09 03:16:46 +0300

    тест входа в админку

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 
@@ -20,6 +22,7 @@ public class MyFirstTest {
     @Before
     public void start() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 20);
 
     }
@@ -28,10 +31,25 @@ public class MyFirstTest {
     public void myFirstTest() {
         driver.get("http://www.google.com/");
         driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
+
+        driver.findElement(By.id("gs_ok0")).click();
+        driver.findElement(By.id("K32")).click();
+        driver.findElement(By.id("gs_ok0")).click();
+
+
+//        driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
+
 //        этот элемент оказывается невидим под "выпадушкой" с вариантами поиска
 //        driver.findElement(By.name("btnK")).click();
+//        <input type="button" value="Поиск в Google" class="lsb">
+//        <div class="sbsb_g" style="width: 581px;"><span class="ds"><span class="lsbb"><input type="button" value="Поиск в Google" class="lsb"></span></span><span class="ds"><span class="lsbb"><input type="button" value="Мне повезёт!" class="lsb"></span></span></div>
 //        sendKeys(Keys.chord(Keys.CONTROL, Keys.C));
+
+//        кнопка-клавиатура
+// <span class="gsok_a gsst_e" id="gs_ok0" style="outline: rgba(0, 0, 255, 0.3) solid 1px;"><img src="//www.gstatic.com/inputtools/images/tia.png" tia_field_name="q" tia_disable_swap="true" tia_property="web"></span>
+
+
+
         wait.until(titleIs("webdriver - Поиск в Google"));
     }
 
